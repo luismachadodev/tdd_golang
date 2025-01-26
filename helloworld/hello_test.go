61e58d48160be6e5d8fc7e7782b4795f77f357ed
Luis Henrique <luis@prover.net.br>
2025-01-26 17:12:01 -0300

    Finalizando fundamentos basicos tdd

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -17,9 +17,15 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("em espanhol", func(t *testing.T) {
-		got := Hello("Luis", "espanhol")
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Luis", "Spanish")
 		want := "Hola, Luis"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Luis", "French")
+		want := "Bonjour, Luis"
 		assertCorrectMessage(t, got, want)
 	})
 }
