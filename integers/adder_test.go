5ffd96c7f2830781cfbd5d713df0ef8eee67e0b9
Luis Henrique <luis@prover.net.br>
2025-01-26 21:11:57 -0300

    Inteiros

---
+++ integers/adder_test.go
@@ -0,0 +1,25 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", sum, expected)
+	}
+}
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
